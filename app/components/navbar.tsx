92d96cef816c31ac146cd9a2814308bce21d9220
Arnold Midalla <midallaarnold@gmail.com>
2025-09-26 14:14:42 +0100

    Added links to navbar - just to push something today

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,11 +9,13 @@ export default async function Navbar() {
   const user = data.user?.user_metadata;
   return (
     <section className="flex fixed h-16 items-center justify-center w-full bg-[#f3f3f0]/20 backdrop-blur-lg z-40 border-b-2">
-      <div>
+      {/* <div>
         <Link href="/">One</Link>
-      </div>
+      </div> */}
       <div>
-        <Link href="/">Two</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href='/login'>Login</Link>
+        <Link href='/signup'>SignUp</Link>
       </div>
       welcome {user?.firstName + " " + user?.lastName}
       {/* <DarkModeButton/> */}
